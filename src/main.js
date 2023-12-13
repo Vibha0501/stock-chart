9c9d5d4c649fb1b078b9d8a03c781513d7b37eea
vibha <alurvibha@gmail.com>
2023-12-13 21:08:58 +0530

    resolved merge conflicts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 // Import Bootstrap CSS globally
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,19 +14,3 @@ app.component("apexchart", VueApexCharts);
 app.use(router)
 
 app.mount('#app')
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import router from './index.js'
-import VueApexCharts from "vue-apexcharts";
-
-Vue.use(VueApexCharts);
-
-Vue.component("apexchart", VueApexCharts);
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
->>>>>>> cabef6739efc2a891d243b7601760c63edec01a4
