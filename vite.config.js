3098e68c95b02345b8091ed3372f6c5728dc701f
vibha <alurvibha@gmail.com>
2023-12-16 16:55:46 +0530

    prod changes

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/stock-chart/" : "/",
-  base: "/",
+  base: "/stock-chart/",
   plugins: [vue()],
   resolve: {
     alias: {
